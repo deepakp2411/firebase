f07712e63bc533f1b2b515ce269094be0fd3f42b
Deepak <bbaz2deepak@gmail.com>
2022-07-23 10:32:47 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react'
 
 import Home from './components/Home'
 import Navbar from './components/navbar/Navbar'
+import Services from './components/services/Services'
 
 const App = () => {
   return (
     <div>
     <Navbar />
       {/* <Home /> */}
+      <Services />
 
   
     </div>
---
+++ src/components/services/Services.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Services = () => {
+  return (
+    <div className='flex justify-center items-center'>
+        <ul className='flex justify-between md:items-center bg-green space-x-5'>
+            <li className='color-green'>Home</li>
+            <li>Home</li>
+            <li>Home</li>
+            <li>Home</li>
+            <li>Home</li>
+            <li>Home</li>
+            <li>Home</li>
+            <li>Home</li>
+            <li>Home</li>
+        </ul>
+    </div>
+  )
+}
+
+export default Services
